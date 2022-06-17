20aea6e4cfd5f5fb796faae6513996516d606dd3
test <test@gmail.com>
2022-06-17 20:14:47 +0100

    upload project

---
+++ js/script.js
@@ -0,0 +1,18 @@
+const ArrowUpBtn = document.querySelector(".arrowBtn"); 
+const header = document.querySelector("header");
+
+ArrowUpBtn.addEventListener('click', ()=>{
+  window.scrollTo({
+    top:0,
+    left:0,
+    behavior: "smooth"
+  });  
+})
+
+if(window != null) {
+    window.addEventListener("scroll", ()=>{
+      var header = this.document.querySelector("header");
+      header.classList.toggle("sticky", window.scrollY > 0);      
+    })
+    
+}
